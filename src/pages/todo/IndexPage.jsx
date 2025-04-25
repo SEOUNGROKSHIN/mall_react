5395977f9ae7c2467edfdb0f9bca44d00faa1460
신승록 <tnsfhr03@gmail.com>
2025-04-25 16:25:07 +0900

    250425 React Todo 마무리

--- src/pages/todo/IndexPage.jsx
+++ src/pages/todo/IndexPage.jsx
@@ -1,11 +1,24 @@
-import { Outlet } from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import BasicLayout from "../../layouts/BasicLayout";
+import {useCallback} from "react";
 const IndexPage = () => {
+
+    const navigate = useNavigate()
+
+    const handlerClickList = useCallback(() => {
+        navigate({pathname:'list'})
+    },[])
+
+    const handlerClickAdd = useCallback(() => {
+        navigate({pathname:'add'})
+    },[])
+
+
     return (
         <BasicLayout>
             <div className="w-full flex m-2 p-2 ">
-                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">LIST</div>
-                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">ADD</div>
+                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handlerClickList}>LIST</div>
+                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handlerClickAdd}>ADD</div>
             </div>
             <div className="flex flex-wrap w-full">
                 <Outlet/>
